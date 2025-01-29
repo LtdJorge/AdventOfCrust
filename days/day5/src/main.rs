963b27946584d9d2661d006bfae8a22f7712d191
Jorge López <jorgekq@hotmail.es>
2025-01-29 15:00:55 +0100

    Solves Day 5 Part 2

--- days/day5/src/main.rs
+++ days/day5/src/main.rs
@@ -2,29 +2,42 @@ mod lexer;
 mod parser;
 mod types;
 
-use crate::{lexer::Token, types::OrderedNumeric};
+use crate::{
+    lexer::Token,
+    types::{HigherNumbers, LowerNumbers, OrderedNumeric, Update},
+};
 use ahash::{HashMap, HashMapExt};
 use chumsky::Parser;
 use common::{get_input, InputType};
 use logos::Logos;
 use std::path::PathBuf;
-use types::{HigherNumbers, LowerNumbers};
 
 fn main() -> anyhow::Result<()> {
     let input_type = InputType::Input(PathBuf::from("./days/day5/input.txt"));
     let part1_result = solve_part_1(input_type.clone())?;
     println!("Part 1: {}", part1_result);
+    let part2_result = solve_part_2(input_type)?;
+    println!("Part 2: {}", part2_result);
 
     Ok(())
 }
 
-fn solve_part_1(input_type: InputType) -> anyhow::Result<usize> {
+pub type OrderingMap = HashMap<usize, (LowerNumbers, HigherNumbers)>;
+
+fn common_part(input_type: InputType) -> anyhow::Result<(Vec<Update>, OrderingMap)> {
     let input = get_input(input_type)?;
     let lexer = Token::lexer(input.as_str());
     let tokens = lexer.map(|lex| lex.unwrap()).collect::<Vec<_>>();
-    let mut map = HashMap::<usize, (LowerNumbers, HigherNumbers)>::new();
     let parsed = parser::parser().parse(tokens.as_slice());
-    let (ordering_rules, updates) = parsed.output().unwrap();
+    let (output, errors) = parsed.into_output_errors();
+
+    if !errors.is_empty() {
+        anyhow::bail!("Parsing errors: {:?}", errors);
+    }
+
+    let (ordering_rules, updates) = output.ok_or(anyhow::anyhow!("No output"))?;
+
+    let mut map = HashMap::<usize, (LowerNumbers, HigherNumbers)>::new();
 
     // Fill up the hashmap
     ordering_rules.iter().for_each(|rule| {
@@ -43,6 +56,12 @@ fn solve_part_1(input_type: InputType) -> anyhow::Result<usize> {
             map.insert(right, (vec![left], vec![]));
         }
     });
+
+    Ok((updates, map))
+}
+
+fn solve_part_1(input_type: InputType) -> anyhow::Result<usize> {
+    let (updates, map) = common_part(input_type)?;
 
     let result = updates
         .iter()
@@ -69,14 +88,57 @@ fn solve_part_1(input_type: InputType) -> anyhow::Result<usize> {
     Ok(result)
 }
 
+fn solve_part_2(input_type: InputType) -> anyhow::Result<usize> {
+    let (mut updates, map) = common_part(input_type)?;
+
+    let result = updates
+        .iter_mut()
+        .filter_map(|update| {
+            let mut numerics = update
+                .list
+                .iter()
+                .map(|item| {
+                    let numeric = map.get(item).unwrap();
+                    OrderedNumeric::new(*item, numeric)
+                })
+                .collect::<Vec<_>>();
+
+            if !numerics.is_sorted() {
+                numerics.sort_unstable();
+                update.list = numerics
+                    .iter()
+                    .map(|ordered_numeric| ordered_numeric.numeric)
+                    .collect();
+
+                Some(update)
+            } else {
+                None
+            }
+        })
+        .map(|update| {
+            let middle = update.list.len() / 2;
+            update.list[middle]
+        })
+        .sum::<usize>();
+
+    Ok(result)
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::solve_part_1;
+    use crate::{solve_part_1, solve_part_2};
     use common::InputType;
 
     #[test]
     fn test_part1() -> anyhow::Result<()> {
         let res = solve_part_1(InputType::Test)?;
+        println!("{}", res);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> anyhow::Result<()> {
+        let res = solve_part_2(InputType::Test)?;
         println!("{}", res);
         Ok(())
     }
